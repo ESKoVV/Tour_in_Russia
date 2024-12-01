29fb6150ce8099d5913bf6ba7e95959c517f8ece
ESKoVV <koksiccotan@gmail.com>
2024-12-01 17:05:32 +0300

    Add files via upload

---
+++ сайт Снежа/main.js
@@ -0,0 +1,11 @@
+
+function TG(){
+    alert('TG')
+}
+function RECORD(){
+    window.open("https://forms.yandex.ru/u/67059aa3c417f3e83466f412/")
+}
+
+function VK(){
+    window.open("https://vk.com/moonacry4")
+}
